e4cfffb220fc9233796605670fec262fd254c2bb
Airing <gx-deng@163.com>
2016-03-23 12:34:02 +0800

    Create bilibili_user_info.sql

---
+++ bilibili_user_info.sql
@@ -0,0 +1,57 @@
+# ************************************************************
+# Sequel Pro SQL dump
+# Version 4135
+#
+# http://www.sequelpro.com/
+# http://code.google.com/p/sequel-pro/
+#
+# Host: 127.0.0.1 (MySQL 5.1.63)
+# Database: python
+# Generation Time: 2016-03-23 04:33:32 +0000
+# ************************************************************
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Dump of table bilibili_user_info
+# ------------------------------------------------------------
+
+CREATE TABLE `bilibili_user_info` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `mid` varchar(11) DEFAULT NULL,
+  `name` varchar(45) DEFAULT NULL,
+  `sex` varchar(11) DEFAULT NULL,
+  `face` varchar(200) DEFAULT NULL,
+  `coins` int(11) DEFAULT NULL,
+  `regtime` varchar(45) DEFAULT NULL,
+  `spacesta` int(11) DEFAULT NULL,
+  `birthday` varchar(45) DEFAULT NULL,
+  `place` varchar(45) DEFAULT NULL,
+  `description` varchar(45) DEFAULT NULL,
+  `article` int(11) DEFAULT NULL,
+  `fans` int(11) DEFAULT NULL,
+  `friend` int(11) DEFAULT NULL,
+  `attention` int(11) DEFAULT NULL,
+  `sign` varchar(300) DEFAULT NULL,
+  `attentions` text,
+  `level` int(11) DEFAULT NULL,
+  `exp` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
