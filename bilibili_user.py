c7e09923d8862bac3b0b3e24f70a8f65fbe3b8a7
Airing <gx-deng@163.com>
2016-09-18 18:08:20 +0800

    针对B站API的反爬虫，进行了代码更新
    1. 这个 url 只能 post
    2. 除了mid还接受了一个随机数参数_，测试之后发现每次请求这个参数的值都不一样，它是当前的时间戳。如1473846755045，它就是你发起请求的那一刻的时间戳（13位精确到毫秒），所以你只要请求的时候，程序生成个时间戳然后封装到 post 里就可以了。
    3. 优化 Header

--- bilibili_user.py
+++ bilibili_user.py
@@ -2,10 +2,16 @@
 
 import requests
 import json
-import time
 import MySQLdb
 from multiprocessing.dummy import Pool as ThreadPool
 import sys
+import datetime
+import time
+
+def datetime_to_timestamp_in_milliseconds(d):
+    current_milli_time = lambda: int(round(time.time() * 1000))
+    return current_milli_time()
+
 
 reload(sys)
 
@@ -14,24 +20,41 @@ sys.setdefaultencoding('utf-8')
 urls = []
 
 head = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.130 Safari/537.36'
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36',
+    'X-Requested-With': 'XMLHttpRequest',
+    'Referer': 'http://space.bilibili.com/873981/',
+    'Origin': 'http://space.bilibili.com',
+    'Host': 'space.bilibili.com',
+    'AlexaToolbar-ALX_NS_PH': 'AlexaToolbar/alx-4.0',
+    'Accept-Language': 'zh-CN,zh;q=0.8,en;q=0.6,ja;q=0.4',
+    'Accept': 'application/json, text/javascript, */*; q=0.01',
 }
 
 time1 = time.time()
-for i in range(1526499, 1526500):
+for i in range(873981, 873982):
     url = 'http://space.bilibili.com/ajax/member/GetInfo?mid=' + str(i)
     urls.append(url)
 
 # s = requests.Session()
 
 proxies = {
-        'http': '120.198.231.87:84',
+        # 'http': '120.198.231.87:84',
         #'https': 'http://219.133.31.120:8888',
 }
 
 
 def getsource(url):
-    jscontent = requests.get(url, headers=head,  proxies=proxies, verify=False).content
+    payload = {
+        '_': datetime_to_timestamp_in_milliseconds(datetime.datetime.now()),
+        'mid': url.replace('http://space.bilibili.com/ajax/member/GetInfo?mid=', '')
+    }
+
+    print payload
+
+    jscontent = requests.post('http://space.bilibili.com/ajax/member/GetInfo', headers=head,  data=payload).content
+
+    print(jscontent)
+
     time2 = time.time()
     jsDict = json.loads(jscontent)
     if jsDict['status'] == True:
@@ -71,7 +94,7 @@ def getsource(url):
         print "Error: " + url
 
 
-pool = ThreadPool(10)
+pool = ThreadPool(1)
 try:
     results = pool.map(getsource, urls)
 except Exception:
