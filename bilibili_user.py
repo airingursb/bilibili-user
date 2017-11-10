849a99872557cc2297634315c3a62c12c1e5d59a
Applenice <30207614+Applenice@users.noreply.github.com>
2017-11-10 10:35:57 +0800

    fix bug

--- bilibili_user.py
+++ bilibili_user.py
@@ -4,19 +4,18 @@ import requests
 import json
 import random
 import pymysql
-from multiprocessing.dummy import Pool as ThreadPool
 import sys
 import datetime
 import time
 from imp import reload
+from multiprocessing.dummy import Pool as ThreadPool
+
 
 def datetime_to_timestamp_in_milliseconds(d):
     def current_milli_time(): return int(round(time.time() * 1000))
     return current_milli_time()
 
-
 reload(sys)
-
 
 def LoadUserAgents(uafile):
     """
@@ -31,7 +30,6 @@ def LoadUserAgents(uafile):
     random.shuffle(uas)
     return uas
 
-
 uas = LoadUserAgents("user_agents.txt")
 head = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36',
@@ -43,17 +41,18 @@ head = {
     'Accept-Language': 'zh-CN,zh;q=0.8,en;q=0.6,ja;q=0.4',
     'Accept': 'application/json, text/javascript, */*; q=0.01',
 }
-proxies = {
-    'http': 'http://124.232.148.7:3128',
-    'http': 'http://119.23.161.182:3128',
-    'http': 'http://45.126.122.117:80',
-    'http': 'http://111.56.5.42:80',
-    'http': 'http://58.17.116.115:53281',
-    'http': 'http://47.91.237.123:8080',
-    'http': 'http://111.56.5.41:80',
-    'http': 'http://139.129.166.68:3128',
-    'http': 'http://118.178.124.33:3128',
-    'http': 'http://45.76.91.218:3128'
+proxies = { 'http': 'http://61.155.164.108:3128',
+            'http': 'http://116.199.115.79:80',
+            'http': 'http://42.245.252.35:80',
+            'http': 'http://106.14.51.145:8118',
+            'http': 'http://116.199.115.78:80',
+            'http': 'http://123.147.165.143:8080',
+            'http': 'http://58.62.86.216:9999',
+            'http': 'http://202.201.3.121:3128',
+            'http': 'http://119.29.201.134:808',
+            'http': 'http://61.155.164.112:3128',
+            'http': 'http://123.57.76.102:80',
+            'http': 'http://116.199.115.78:80',
 }
 time1 = time.time()
 for m in xrange(99, 101):  # 26 ,1000
@@ -75,7 +74,6 @@ for m in xrange(99, 101):  # 26 ,1000
         time2 = time.time()
         try:
             jsDict = json.loads(jscontent)
-            print(jsDict)
             statusJson = jsDict['status'] if 'status' in jsDict.keys(
             ) else False
             if statusJson == True:
